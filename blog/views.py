1772a867b1e11cb0b34b4c3f85bf9c5e18a27692
insertdataplz <pipin4682@gmail.com>
2022-10-26 16:40:14 +0900

    2022-10-26수업 끝

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def index(request):
+    # 전체 포스팅을 가져올 준비. 아직 가져오진 않음
+    post_qs = Post.objects.all().order_by("-id")
+    return render(request, "blog/index.html", {
+        "post_list": post_qs,
+    })
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {
+        'post': post
+    })
